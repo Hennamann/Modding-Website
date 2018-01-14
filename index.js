e5ef4dc0f58deb699e3f5a198dc89e730c8a385e
Ole Henrik Stabell <ole@henrikstabell.com>
2018-01-14 16:48:42 +0100

    Added release pages for nmclc and wasteland

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
 app.get('/mods', (req, res) => res.sendFile(__dirname + '/views/mods.html'))
 app.get('/mods/mistcore/releases', (req, res) => res.sendFile(__dirname + '/views/mods/mcreleases.html'))
 app.get('/mods/mistbiomes/releases', (req, res) => res.sendFile(__dirname + '/views/mods/mbreleases.html'))
+app.get('/mods/nmclc/releases', (req, res) => res.sendFile(__dirname + '/views/mods/nmclcreleases.html'))
+app.get('/mods/wasteland/releases', (req, res) => res.sendFile(__dirname + '/views/mods/wrreleases.html'))
 
 app.use("*", function (req, res) {
   res.sendFile(__dirname + "/views/404.html");
